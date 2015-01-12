ce95d55a418694b04491892c2cd4bc242680e4a2
Phúc <nguyen.phuc@mulodo.com>
2015-01-12 17:03:40 +0700

    19556: Login + Logout + Home

---
+++ src/MiniBlog/src\main\java/com/blog/model/User.java
@@ -0,0 +1,26 @@
+package com.blog.model;
+
+import java.sql.Date;
+
+public class User {
+ private int userID;
+ private String userName;
+ private String passWord;
+ private String firstName;
+ private String lastName;
+ private String avatar;
+ private int gender;
+ private Date birthday;
+ private String address;
+ private String city;
+ private String email;
+ private String mobile;
+ private int group;
+ private int lastLogin;
+ private String loginHash;
+ private Date createAt;
+ private Date updateAt;
+
+ 
+ 
+}
