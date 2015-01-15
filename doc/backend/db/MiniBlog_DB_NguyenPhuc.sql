0c6b0b0154e40044101633c29b7017b0a9094e69
Phúc <nguyen.phuc@mulodo.com>
2015-01-15 09:36:40 +0700

    Data base design
    https://my.redmine.jp/mulodo/issues/19493

---
+++ doc/backend/db/MiniBlog_DB_NguyenPhuc.sql
@@ -0,0 +1,140 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.2.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:8889
+-- Generation Time: Jan 15, 2015 at 03:34 AM
+-- Server version: 5.5.38
+-- PHP Version: 5.6.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `mini_blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+`id` int(11) NOT NULL,
+  `content` varchar(250) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE `post` (
+`id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` tinytext NOT NULL,
+  `content` longtext NOT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+`id` int(11) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `avatar` varchar(100) DEFAULT NULL,
+  `gender` tinyint(4) NOT NULL DEFAULT '1',
+  `birthday` date NOT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `mobile` varchar(20) DEFAULT NULL,
+  `group` int(11) NOT NULL DEFAULT '0',
+  `last_login` int(11) NOT NULL,
+  `login_hash` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `firstname`, `lastname`, `avatar`, `gender`, `birthday`, `address`, `city`, `email`, `mobile`, `group`, `last_login`, `login_hash`, `created_at`, `updated_at`) VALUES
+(1, 'phucios', '123456', 'phúc', 'nguyen', NULL, 1, '2015-01-10', 'dấdad', 'aaaa', 'aaa@gmail.com', '2324234', 0, 0, NULL, '2013-12-02 05:19:37', '2015-01-13 03:13:16'),
+(2, 'phuc', '123456', 'sadasd', 'aaa', 'aaaa', 1, '2015-01-12', NULL, NULL, '', NULL, 0, 0, NULL, '2015-01-12 00:00:00', '2015-01-12 00:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+ ADD PRIMARY KEY (`id`), ADD KEY `user_id` (`user_id`), ADD KEY `post_id` (`post_id`);
+
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+ ADD PRIMARY KEY (`id`), ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
